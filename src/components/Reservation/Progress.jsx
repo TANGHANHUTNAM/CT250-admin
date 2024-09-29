7e75dfa79d2d5cf4f6e61f072234cc976ccb84bd
akjtjhklf <trngtran358@gmail.com>
2024-09-29 15:13:34 +0700

    update mutil language

--- src/components/Reservation/Progress.jsx
+++ src/components/Reservation/Progress.jsx
@@ -1,15 +1,24 @@
+import { useTranslation } from "react-i18next";
 import ReservationItem from "./ReservationItem";
 
 const Progress = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="basis-12/12 lg:basis-4/12">
       <div className="flex flex-col border-2 border-neutral-400 justify-center items-center p-6 rounded-lg">
         <div className="bg-neutral-200 rounded-r-lg">
           <button className="bg-violet-700 text-white font-simebold p-2 !px-4 rounded-l-lg">
-            Pending
+            {t("Reservation.status.status1")}
           </button>
-          <button className="p-2 !px-4">Accepted</button>
-          <button className="p-2 !px-4 !rounded-r-lg">Denied</button>
+          <button className="p-2 !px-4">
+            {" "}
+            {t("Reservation.status.status2")}
+          </button>
+          <button className="p-2 !px-4 !rounded-r-lg">
+            {" "}
+            {t("Reservation.status.status3")}
+          </button>
         </div>
         <ReservationItem />
       </div>
